4b08e3e0cf733845f26450fc39a6da4871f3c9f4
nidhi <nidhi@172.28.99.134>
2019-08-13 12:35:46 +1200

    Initial commit second commit

--- src/petgame/Pets.java
+++ src/petgame/Pets.java
@@ -1,7 +1,7 @@
+package petgame;
 
-import static petgame.Levels.energy;
-import static petgame.Levels.full;
-import static petgame.Levels.mood;
+
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,55 +17,48 @@ public abstract class Pets
 {
     public static String name;
     public static int age;
-    int mood;
-    int energy;
-    int full;
+    int mood,energy,full,eat,sleep,play,talk;
+   
    
     
     public Pets (String name,int age)
     {
        Pets.name = name;
        Pets.age = age;
+       
+    }
+    public Pets()
+    {
+        this.eat = 0;
+        this.sleep = 0;
+        this.play = 0;
+        this.talk = 0;
     }
     
     public void feed ()
     {
-        
-        System.out.println("Thank you");
+    
     }
     public void sleep ()
     {
-        System.out.println("ZZzzzzz");
+        
     }
     
     public void play ()
     {
-        System.out.println("This is so fun!");
+       
     }
     public abstract void talk ();
+
+    @Override
+    public String toString() {
+        return "Pets{" + "mood=" + this.mood + ", energy=" + this.energy + ", full=" + this.full + ", eat=" + this.eat + ", sleep=" + this.sleep + ", play=" + this.play + ", talk=" + this.talk + '}';
+    }
     
-     public int Fullness(double min, double max)
-    {
-        
-       full=(int) ((int) (Math.random()*((max-min)+1))+min);
-       System.out.print(" Fullness:" +full);
-       return full;
-        
-    }
-     public int Mood(double min, double max)
-    {
-       mood=(int) ((int) (Math.random()*((max-min)+1))+min);
-       System.out.print(" Mood:" +mood);
-       return mood;
-        
-    }
-     public int Energy(double min, double max)
-    {
-       energy=(int) ((int) (Math.random()*((max-min)+1))+min);
-       System.out.print(" Energy:" +energy);
-       return energy;
-        
-    }
+     
+     
+    
+
             
     
 }
