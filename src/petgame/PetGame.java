5ad70404fec500d9cf5ff7b738dcefb761ede9a7
nidhi <nidhi@172.28.99.134>
2019-08-18 15:33:06 +1200

    Initial commit second commit third commit fourth commit fifth commit

--- src/petgame/PetGame.java
+++ src/petgame/PetGame.java
@@ -1,75 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package petgame;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
-/**
- *
- * @author nidhi
- */
+
 public class PetGame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        
         Scanner scan = new Scanner (System.in);
-        System.out.println("Choose your pet by the number");
+        
+        Dog d = new Dog("bruno",6);
+        Cat c = new Cat("candy",2);
+        Rabbit r = new Rabbit("Rocky",1);
+        GuineaPig g = new GuineaPig("Sally",3);
+        System.out.println("What kind of pet do you want:");
+        File file = new File("C:\\Users\\nidhi\\Documents\\NetBeansProjects\\PetGame\\Pets.txt"); 
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+        String st; 
+        while ((st = br.readLine()) != null) 
+        System.out.println(st); 
+        
         int choice = scan.nextInt();
-        int c = scan.nextInt();
+    
         String choice2 = scan.nextLine();
         if (choice ==1||choice2.equalsIgnoreCase("Dog"))
         {
-            System.out.println("Hello owner!I'm your friend doggie");
-            
+            System.out.println("Hello owner!I'm your friend "+Dog.name+"the Dog");
+            d.DogInfo();
         }
         if (choice ==2||choice2.equals("Cat"))
         {
-            System.out.println("Hello owner!I'm your friend doggie");
+            System.out.println("Hello owner!I'm your friend "+Cat.name+ "the Cat");
+            c.CatInfo();
         }
          if (choice ==3||choice2.equals("Rabbit"))
         {
-            System.out.println("Hello owner!I'm your friend doggie");
+            System.out.println("Hello owner!I'm your friend "+Rabbit.name+ "the Rabit");
+            r.RabbitInfo();
         }
          if (choice ==4||choice2.equals("Guinea pig"))
         {
-            System.out.println("Hello owner!I'm your friend doggie");
+            System.out.println("Hello owner!I'm your friend "+GuineaPig.name+ "the Guinea pig");
+            g.GuineaInfo();
         }
-//        Levels level = new Levels();
-////        level.Fullness(50,100);
-////        level.Energy(50, 100);
-////        level.Mood(50, 100);
-      Dog d = new Dog();
-//        d.feed();
-//        level.Energy(10, 100);
+    }
+}
 
-        System.out.println("What would you like your pet to do: 1.Feed it 2.Play 3.Sleep 4.Talk");
-        if(c ==1)
-        {
-            d.feed();
-        }
-        if(c == 2)
-        {
-            d.play();
-        }
-        if(c == 3)
-        {
-            d.sleep();
-        }
-        if(c == 4)
-        {
-            d.talk();
-        }
-    };
-        //d.play();
-        
-
-}//
     
 
